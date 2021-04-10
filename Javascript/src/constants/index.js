c9bc21d2299ee8dd49031702a012538f4a37c111
Wellington Araujo <wellingtonaraujosoares@gmail.com>
2021-04-10 01:54:32 -0300

    Removed Javascript implementation

--- Javascript/src/constants/index.js
+++
@@ -1,4 +0,0 @@
-export const GOAL_STATE = ['1','2','3','4','5','6','7','8','*'];
-// export const GOAL_STATE = ['*','1','2','3','4','5','6','7','8'];
-// export const INITIAL_STATE = ['1','2','3','4','5','6','*','7','8'];
-export const INITIAL_STATE = ['2','*','4','3','1','6','7','5','8'];
